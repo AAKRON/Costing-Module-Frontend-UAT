2dd0758d51f145968f511f3c58262209d1d7a471
AAKRON <ITDEPARTMENT@AAKRONLINE.COM>
2026-07-08 07:48:58 -0400

    Port inks resource from production

---
+++ src/resources/inks/InkEdit.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { DeleteButton, Edit, ListButton, NumberInput, SaveButton, SimpleForm, TextInput, Toolbar, TopToolbar, required } from 'react-admin'
+import { isModificationPermission } from '../../helpers/functions'
+
+const Actions = () => (
+  <TopToolbar>
+      <ListButton />
+  </TopToolbar>
+)
+
+const ToolbarForm = (props) => {
+  if(!isModificationPermission()) return false
+
+  return (
+    <Toolbar {...props}>
+      <SaveButton />
+      <DeleteButton mutationMode="pessimistic" />
+    </Toolbar>
+  )
+}
+
+export const InkEdit = (props) => (
+  <Edit
+    actions={<Actions />}
+    {...props}
+  >
+    <SimpleForm
+      toolbar={<ToolbarForm />}
+    >
+      <TextInput fullWidth source='id' disabled validate={required()}/>
+      <TextInput fullWidth source='name' validate={required()}/>
+      <NumberInput fullWidth source='ink_cost' label='Ink Cost($)' validate={required()}/>
+    </SimpleForm>
+  </Edit>
+)
