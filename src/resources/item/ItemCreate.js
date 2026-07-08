5d84b980ffe93a87c3a4a5ec2ef635a3096e7262
AAKRON <ITDEPARTMENT@AAKRONLINE.COM>
2026-07-08 08:11:24 -0400

    Add ink_id dropdown to ItemEdit and ItemCreate

--- src/resources/item/ItemCreate.js
+++ src/resources/item/ItemCreate.js
@@ -4,118 +4,105 @@ import { isModifyPermission } from '../../helpers/functions'
 import restClient from '../../providers/restClient'
 
 const ItemCreate = (props) => {
-	if(!isModifyPermission()){
-    return null
-  }
+	if (!isModifyPermission()) {
+		return null
+	}
 
-  const [redirectTo, setRedirectTo] = useState('list')
-  const redirect = useRedirect()
-  const notify = useNotify()
-  const resource = useResourceContext()
+	const [redirectTo, setRedirectTo] = useState('list')
+	const redirect = useRedirect()
+	const notify = useNotify()
+	const resource = useResourceContext()
 
-	const [boxes, setBoxes] = useState({
-		loading: true,
-		data: []
-	})
-	const [itemsType, setItemsType] = useState({
-		loading: true,
-		data: []
-	})
+	const [boxes, setBoxes] = useState({ loading: true, data: [] })
+	const [itemsType, setItemsType] = useState({ loading: true, data: [] })
+	const [inks, setInks] = useState({ loading: true, data: [] })
 
-	const fetchBoxes = () => restClient.getList('box-list-only', {pagination: { page: 1, perPage: -1 }, sort: { field: 'id', order: 'ASC' }})
-
-	const fetchItemTypes = () => restClient.getList('item-type-list-only', {pagination: { page: 1, perPage: -1 }, sort: { field: 'id', order: 'ASC' }})
+	const fetchBoxes = () => restClient.getList('box-list-only', { pagination: { page: 1, perPage: -1 }, sort: { field: 'id', order: 'ASC' } })
+	const fetchItemTypes = () => restClient.getList('item-type-list-only', { pagination: { page: 1, perPage: -1 }, sort: { field: 'id', order: 'ASC' } })
+	const fetchInks = () => restClient.getList('ink-list-only', { pagination: { page: 1, perPage: -1 }, sort: { field: 'id', order: 'ASC' } })
 
 	useEffect(() => {
-		fetchBoxes().then(({data}) => {
-			const boxes = data.map(box => ({id: box.id, name: box.name}));
-			setBoxes({ loading: false, data: boxes })
-		}).catch((err) => {
-			console.log('Error fetching boxes', err)
-		})
+		fetchBoxes().then(({ data }) => {
+			setBoxes({ loading: false, data: data.map(b => ({ id: b.id, name: b.name })) })
+		}).catch(err => console.log('Error fetching boxes', err))
 
-		fetchItemTypes().then(({data}) => {
-			const item_types = data.map(box => ({id: box.type_number, name: box.description}));
-			setItemsType({ loading: false, data: item_types })
-		}).catch((err) => {
-			console.log('Error fetching item types', err)
-		})
+		fetchItemTypes().then(({ data }) => {
+			setItemsType({ loading: false, data: data.map(t => ({ id: t.type_number, name: t.description })) })
+		}).catch(err => console.log('Error fetching item types', err))
+
+		fetchInks().then(({ data }) => {
+			setInks({ loading: false, data: data.map(i => ({ id: i.id, name: i.name })) })
+		}).catch(err => console.log('Error fetching inks', err))
 	}, [])
 
 	const Actions = () => (
 		<TopToolbar>
-				<ListButton />
+			<ListButton />
 		</TopToolbar>
 	)
-	
+
 	const ToolbarForm = (props) => {
-    return (
-      <Toolbar {...props}>
-        <SaveButton
-          onClick={() => {
-            setRedirectTo('list')
-          }}
-        />
+		return (
+			<Toolbar {...props}>
+				<SaveButton onClick={() => setRedirectTo('list')} />
+				<SaveButton
+					label='Save and Add'
+					sx={{ mx: '1em' }}
+					onClick={() => setRedirectTo('create')}
+				/>
+			</Toolbar>
+		)
+	}
 
-        <SaveButton
-          label='Save and Add'
-          sx={{ mx: '1em' }}
-          onClick={() => {
-            setRedirectTo('create')
-          }}
-        />
-      </Toolbar>
-    )
-  }
-
-	return(
+	return (
 		<Create
 			mutationOptions={{
 				onSuccess: (data) => {
 					notify(`Item ${data.id} has been created`)
-
-					if(redirectTo === 'create') {
+					if (redirectTo === 'create') {
 						redirect(redirectTo, resource)
-
-						setTimeout(() => {
-							window.location.reload()
-						}, 500)
+						setTimeout(() => window.location.reload(), 500)
 					}
-
-					if(redirectTo === 'list') {
+					if (redirectTo === 'list') {
 						redirect(redirectTo, resource)
 					}
 				},
-				onError: () => {
-					notify('No item has been created, please try again', 'warning')
-				}
+				onError: () => notify('No item has been created, please try again', 'warning')
 			}}
 			actions={<Actions />}
 			{...props}
 		>
-			<SimpleForm
-				toolbar={<ToolbarForm />}
-			>
-				<NumberInput source='item_number' validate={required()}/>
-				<TextInput source='description' validate={required()}/>
+			<SimpleForm toolbar={<ToolbarForm />}>
+				<NumberInput fullWidth source='item_number' validate={required()} />
+				<TextInput fullWidth source='description' validate={required()} />
 				<AutocompleteInput
+					fullWidth
 					isLoading={boxes.loading}
 					source='box_id'
 					choices={boxes.data}
 				/>
-				<NumberInput source='number_of_pcs_per_box' label='Number Of PCS/Box' validate={required()}/>
+				<NumberInput fullWidth source='number_of_pcs_per_box' label='Number Of PCS/Box' validate={required()} />
 				<AutocompleteInput
+					fullWidth
 					isLoading={boxes.loading}
 					source='secondary_box_id'
 					label='Secondary Box'
 					choices={boxes.data}
 				/>
-				<NumberInput source='number_of_pcs_per_secondary_box' label='Number Of PCS/Secondary Box'/>
+				<NumberInput fullWidth source='number_of_pcs_per_secondary_box' label='Number Of PCS/Secondary Box' />
 				<AutocompleteInput
+					fullWidth
 					isLoading={itemsType.loading}
 					source='item_type_id'
 					choices={itemsType.data}
 				/>
+				<AutocompleteInput
+					fullWidth
+					isLoading={inks.loading}
+					source='ink_id'
+					choices={inks.data}
+				/>
+				<NumberInput fullWidth source='ink_cost' label='Ink Cost($)' />
 			</SimpleForm>
 		</Create>
 	)
--- src/resources/item/ItemEdit.js
+++ src/resources/item/ItemEdit.js
@@ -1,32 +1,28 @@
 import { useEffect, useState } from 'react'
-import { AutocompleteInput, DeleteButton, Edit, ListButton, NumberInput, SaveButton, SimpleForm, TextInput, Toolbar, TopToolbar, required } from 'react-admin'
+import { AutocompleteInput, DeleteButton, Edit, ListButton, NumberInput, SaveButton, SimpleForm, TextInput, Toolbar, TopToolbar, required, useEditController } from 'react-admin'
 import { isModifyPermission } from '../../helpers/functions'
 import restClient from '../../providers/restClient'
 import ItemCostView from './ItemCostView'
 
 export const ItemEdit = (props) => {
-	const [boxes, setBoxes] = useState({
-		loading: true,
-		data: []
-	})
-	const [itemsType, setItemsType] = useState({
-		loading: true,
-		data: []
-	})
+	const { record } = useEditController(props)
 
-	const fetchBoxes = () => restClient.getList('box-list-only', {pagination: { page: 1, perPage: -1 }, sort: { field: 'id', order: 'ASC' }})
+	const [boxes, setBoxes] = useState({ loading: true, data: [] })
+	const [itemsType, setItemsType] = useState({ loading: true, data: [] })
+	const [inks, setInks] = useState({ loading: true, data: [] })
 
-	const fetchItemTypes = () => restClient.getList('item-type-list-only', {pagination: { page: 1, perPage: -1 }, sort: { field: 'id', order: 'ASC' }})
+	const fetchBoxes = () => restClient.getList('box-list-only', { pagination: { page: 1, perPage: -1 }, sort: { field: 'id', order: 'ASC' } })
+	const fetchItemTypes = () => restClient.getList('item-type-list-only', { pagination: { page: 1, perPage: -1 }, sort: { field: 'id', order: 'ASC' } })
+	const fetchInks = () => restClient.getList('ink-list-only', { pagination: { page: 1, perPage: -1 }, sort: { field: 'id', order: 'ASC' } })
 
 	const Actions = () => (
-    <TopToolbar>
-        <ListButton />
-    </TopToolbar>
-  )
+		<TopToolbar>
+			<ListButton />
+		</TopToolbar>
+	)
 
 	const ToolbarForm = (props) => {
-		if(!isModifyPermission()) return false
-	
+		if (!isModifyPermission()) return false
 		return (
 			<Toolbar {...props}>
 				<SaveButton />
@@ -36,19 +32,17 @@ export const ItemEdit = (props) => {
 	}
 
 	useEffect(() => {
-		fetchBoxes().then(({data}) => {
-			const boxes = data.map(box => ({id: box.id, name: box.name}));
-			setBoxes({ loading: false, data: boxes })
-		}).catch((err) => {
-			console.log('Error fetching boxes', err)
-		})
+		fetchBoxes().then(({ data }) => {
+			setBoxes({ loading: false, data: data.map(b => ({ id: b.id, name: b.name })) })
+		}).catch(err => console.log('Error fetching boxes', err))
 
-		fetchItemTypes().then(({data}) => {
-			const item_types = data.map(box => ({id: box.type_number, name: box.description}));
-			setItemsType({ loading: false, data: item_types })
-		}).catch((err) => {
-			console.log('Error fetching item types', err)
-		})
+		fetchItemTypes().then(({ data }) => {
+			setItemsType({ loading: false, data: data.map(t => ({ id: t.type_number, name: t.description })) })
+		}).catch(err => console.log('Error fetching item types', err))
+
+		fetchInks().then(({ data }) => {
+			setInks({ loading: false, data: data.map(i => ({ id: i.id, name: i.name })) })
+		}).catch(err => console.log('Error fetching inks', err))
 	}, [])
 
 	return (
@@ -56,33 +50,43 @@ export const ItemEdit = (props) => {
 			{...props}
 			actions={<Actions />}
 		>
-			<SimpleForm
-				toolbar={<ToolbarForm />}
-			>
-				<TextInput source='id' disabled validate={required()}/>
-				<NumberInput source='item_number' validate={required()}/>
-				<TextInput multiline source='description' validate={required()}/>
+			<SimpleForm toolbar={<ToolbarForm />}>
+				<TextInput source='id' disabled validate={required()} />
+				<NumberInput source='item_number' validate={required()} />
+				<TextInput multiline source='description' validate={required()} />
 				<AutocompleteInput
+					fullWidth
 					isLoading={boxes.loading}
 					source='box_id'
 					choices={boxes.data}
 				/>
-				<NumberInput source='number_of_pcs_per_box' label='Number Of PCS/Box' validate={required()}/>
+				<NumberInput source='number_of_pcs_per_box' label='Number Of PCS/Box' validate={required()} />
 				<AutocompleteInput
+					fullWidth
 					isLoading={boxes.loading}
 					source='secondary_box_id'
 					label='Secondary Box'
 					choices={boxes.data}
 				/>
-				<NumberInput source='number_of_pcs_per_secondary_box' label='Number Of PCS/Secondary Box'/>
+				<NumberInput source='number_of_pcs_per_secondary_box' label='Number Of PCS/Secondary Box' />
 				<AutocompleteInput
+					fullWidth
 					source='item_type_id'
 					choices={itemsType.data}
 					isLoading={itemsType.loading}
 				/>
-				<NumberInput source='ink_cost' label='Ink Cost($)' validate={required()}/>
-				<ItemCostView type='price'/>
-				<ItemCostView type='inventory'/>
+				<AutocompleteInput
+					fullWidth
+					source='ink_id'
+					choices={inks.data}
+					isLoading={inks.loading}
+					helperText="If no ink is selected, the ink_cost field will be used for calculations"
+				/>
+				{!record?.ink_id && (
+					<NumberInput fullWidth source='ink_cost' label='Ink Cost($)' />
+				)}
+				<ItemCostView type='price' />
+				<ItemCostView type='inventory' />
 			</SimpleForm>
 		</Edit>
 	)
